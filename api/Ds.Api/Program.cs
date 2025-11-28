93276d12ecee278e840449b062d2664c652aecec
Francesco Zorzi <francesco.zorzi@iasonltd.com>
2025-11-28 17:21:35 +0100

    fix: enable CORS in development environment for API

--- api/Ds.Api/Program.cs
+++ api/Ds.Api/Program.cs
@@ -33,6 +33,7 @@ using (var scope = app.Services.CreateScope())
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseCors(cpb => cpb.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 }
 
 app.UseSerilogRequestLogging();
